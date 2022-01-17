d5f588edc5aa84f9fa92d8638721ba259f8e8a8b
Daniel Yuschick <daniel.yuschick@supermetrics.com>
2022-01-17 16:11:41 +0200

    feat(typography) add typography mixin

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,6 +2,8 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
+    fontSize?: Record<string | number, string> | string[];
+    fontWeight?: Record<string | number, string> | string[];
     radii?: Record<string | number, string> | string[];
     shadow?: Record<string | number, string> | string[];
     space?: Record<string | number, string> | string[];
